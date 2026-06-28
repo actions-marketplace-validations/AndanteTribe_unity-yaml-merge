d784dc19f676368efb6eff4a0a46963d58bb2397
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-06-29 00:04:23 +0900

    fix: Disable automatic editing during merge continuation in GitHelper for better control

--- src/UnityYamlMerge.Git/GitHelper.cs
+++ src/UnityYamlMerge.Git/GitHelper.cs
@@ -209,6 +209,7 @@ public static class GitHelper
         var processStartInfo = ProcessStartInfo.Create("git");
         processStartInfo.ArgumentList.Add("merge");
         processStartInfo.ArgumentList.Add("--continue");
+        processStartInfo.Environment["GIT_MERGE_AUTOEDIT"] = "no";
 
         var output = new ConcurrentQueue<string>();
         var exitCode = await Process.StartAsync(processStartInfo, output, cancellationToken);
